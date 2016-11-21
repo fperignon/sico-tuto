18076e7b509deea68d1f527344a83b5a65702971
Franck Pérignon <franck.perignon@imag.fr>
2016-11-21 22:42:56 +0100

    update options

--- siconos_default.cmake
+++ siconos_default.cmake
@@ -13,9 +13,9 @@
 
 # --------- User-defined options ---------
 # Use cmake -DOPTION_NAME=some-value ... to modify default value.
-option(WITH_DOCUMENTATION "Build Documentation. Default = OFF" ON)
+option(WITH_DOCUMENTATION "Build Documentation. Default = OFF" OFF)
 option(WITH_PYTHON_WRAPPER "Build python bindings using swig. Default = ON" ON)
-option(WITH_DOXYGEN_WARNINGS "Explore doxygen warnings." ON)
+option(WITH_DOXYGEN_WARNINGS "Explore doxygen warnings." OFF)
 option(WITH_DOXY2SWIG "Build swig docstrings from doxygen xml output. Default = OFF." ON)
 option(WITH_SYSTEM_INFO "Verbose mode to get some system/arch details. Default = off." OFF)
 option(WITH_TESTING "Enable 'make test' target" ON)
@@ -26,12 +26,12 @@ option(WITH_CXX "Enable CXX compiler for Numerics. Default=ON." ON)
 option(WITH_UNSTABLE "Enable this to include all 'unstable' sources. Default=OFF" OFF)
 option(BUILD_SHARED_LIBS "Building of shared libraries" ON)
 option(DEV_MODE "Compilation flags setup for developpers. Default: ON" ON)
-option(WITH_BULLET "compilation with Bullet Bindings. Default = OFF" ON)
+option(WITH_BULLET "compilation with Bullet Bindings. Default = OFF" OFF)
 option(WITH_OCC "compilation with OpenCascade Bindings. Default = OFF" OFF)
 option(WITH_MUMPS "Compilation with MUMPS solver. Default = OFF" OFF)
 option(WITH_FCLIB "link with fclib when this mode is enable. Default = off." OFF)
 option(WITH_FREECAD "Use FreeCAD" OFF)
-option(WITH_MECHANISMS "Generation of bindings for Saladyn Mechanisms toolbox" ON)
+option(WITH_MECHANISMS "Generation of bindings for Saladyn Mechanisms toolbox" OFF)
 option(WITH_XML "Enable xml files i/o. Default = ON" ON)
 
 # Set python install mode:
@@ -44,5 +44,5 @@ set(siconos_python_install "user" CACHE STRING "Install mode for siconos python 
 # List of components to build and installed
 # List of siconos component to be installed
 #set(COMPONENTS externals numerics kernel CACHE INTERNAL "List of siconos components to build and install")
-set(COMPONENTS externals numerics kernel control mechanics io CACHE INTERNAL "List of siconos components to build and install")
+set(COMPONENTS externals numerics kernel control CACHE INTERNAL "List of siconos components to build and install")
 #set(COMPONENTS_DIRS externals Numerics CACHE INTERNAL "List of siconos components to build and install")
